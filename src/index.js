2620925289bca68af68cac5c96b9504190194bbc
Jiggy Brillantes <brillantes.git@gmail.com>
2021-06-25 11:35:25 +0800

    Clean code

--- src/index.js
+++ src/index.js
@@ -1,43 +1,58 @@
 import key from "../config";
 
-const myKey = key;
+const APIKEY = key;
 
 const form = document.querySelector('.location-form');
 const input = document.getElementById('location');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    renderAll();
+    showWeather().catch(error => {
+        console.error(error);
+    });
 })
 
 async function fetchData() {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${myKey}&q=${input.value}`, {mode: 'cors'});
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${input.value}`, {mode: 'cors'});
     return response.json();
 }
 
-function getData(data) {
+function getWeather(data) {
     const weather = {
-        place: {
+        location: {
             city: data.location.region,
             country: data.location.country
         },
         temperature: {
-            temp: data.current.temp_c,
-            feelsLike: data.current.feelslike_c
-        }
+            tempC: data.current.temp_c,
+            feelsLikeC: data.current.feelslike_c,
+            tempF: data.current.temp_f,
+            feelsLikeF: data.current.feelslike_f
+        },
+        wind: {
+            windMPH: data.current.wind_mph,
+            windKPH: data.current.wind_kph
+        },
+        condition: {
+            text: data.current.condition.text,
+            icon: data.current.condition.icon
+        },
+        humidity: data.current.humidity,
     }
     return weather;
 }
 
-async function renderAll() {
+async function showWeather() {
     const data = await fetchData().catch(error => {
         console.error(error);
     });
     if (data.error) return;
     console.log(data);
-    console.log(getData(data));
-    input.value = null;
+    console.log(getWeather(data));
+    changeInput();
 }
 
-// ERROR HANDLING
-
+function changeInput() {
+    input.placeholder = input.value;
+    input.value = null;
+}
